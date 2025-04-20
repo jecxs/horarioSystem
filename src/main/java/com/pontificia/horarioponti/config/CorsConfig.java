98761c5b0ec391b2468468200113dc9fe3c1466e
lewis <luisangelcalleallcca@gmail.com>
2025-04-20 16:39:53 -0500

    feat: CorsConfig corregido, se quito la ruta de recepcion de solicitudes de peticiones, se implementera segun lo requerido

--- src/main/java/com/pontificia/horarioponti/config/CorsConfig.java
+++ src/main/java/com/pontificia/horarioponti/config/CorsConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${app.cors.allowed-origins:http://localhost:4200}")
+    @Value("${app.cors.allowed-origins}")
     private String[] allowedOrigins;
 
     @Bean
