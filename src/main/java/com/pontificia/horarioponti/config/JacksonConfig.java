d1b478be0ee6c89277550783f4ba2cfc9c17c392
lewis <luisangelcalleallcca@gmail.com>
2025-04-20 16:54:21 -0500

    feat: subida a la carpeta config

--- src/main/java/com/pontificia/horarioponti/config/JacksonConfig.java
+++ src/main/java/com/pontificia/horarioponti/config/JacksonConfig.java
@@ -1,4 +1,4 @@
-package com.pontificia.horarioponti;
+package com.pontificia.horarioponti.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.SerializationFeature;
